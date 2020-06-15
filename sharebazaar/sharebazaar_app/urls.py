545ca284111d6cd97c3875701e96c3a1338e5496
apurvchaudhary <apurv.sirohi@gmail.com>
2020-06-16 00:59:34 +0530

    url

--- sharebazaar/sharebazaar_app/urls.py
+++ sharebazaar/sharebazaar_app/urls.py
@@ -4,7 +4,7 @@ from sharebazaar_app.views import HomePageView, DayPageView, MonthPageView, NetW
     FilterPageView, SortingPageView
 
 urlpatterns = [
-    re_path('', HomePageView.as_view(), name="home"),
+    re_path(r'^home/$', HomePageView.as_view(), name="home"),
     re_path(r'^day/$', DayPageView.as_view(), name="day"),
     re_path(r'^month/$', MonthPageView.as_view(), name="month"),
     re_path(r'^networth/$', NetWorthPageView.as_view(), name="networth"),
